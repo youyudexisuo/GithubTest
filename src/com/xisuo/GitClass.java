34542221db60a7d08dffd9b14ae11f1f6b500f5c
youyudexisuo <1198596144@qq.com>
2013-08-08 16:35:36 +0800

    first commit by youyudexisuo

---
+++ src/com/xisuo/GitClass.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+package com.xisuo;
+
+//类描述:
+//作者:忧郁的西索 ★^_^+
+public class GitClass {
+	String s="git";
+}
---
+++ src/com/xisuo/GithubClass.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package com.xisuo;
+
+//类描述:
+//作者:忧郁的西索 ★^_^+
+public class GithubClass {
+
+	String s="github";
+}
